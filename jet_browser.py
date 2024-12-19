d66bb5aa1233280185b580515cc2e38a780a3dd9
raihanbairuny <114376707+raihanbairuny@users.noreply.github.com>
2024-12-19 19:55:09 +0700

    Update jet_browser.py

--- jet_browser.py
+++ jet_browser.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 import streamlit as st
+from openpyxl import Workbook
 
 pd.set_option('display.max_columns', None)
 
